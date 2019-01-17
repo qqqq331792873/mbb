f4a267a50fa7fd8bfa0905d35d145f6e3024b380
qqqq331792873 <33510917+qqqq331792873@users.noreply.github.com>
2019-01-17 15:46:48 +0800

    自动生成实体的配置文件
    这个配置文件仅限于IDEA使用,使用方法自行尝试

---
+++ Generate POJOs.groovy
@@ -0,0 +1,81 @@
+import com.intellij.database.model.DasTable
+import com.intellij.database.util.Case
+import com.intellij.database.util.DasUtil
+
+/*
+ * Available context bindings:
+ *   SELECTION   Iterable<DasObject>
+ *   PROJECT     project
+ *   FILES       files helper
+ */
+
+packageName = ""
+typeMapping = [
+  (~/(?i)int/)                      : "long",
+  (~/(?i)float|double|decimal|real/): "double",
+  (~/(?i)datetime|timestamp/)       : "long",
+  (~/(?i)date/)                     : "long",
+  (~/(?i)time/)                     : "long",
+  (~/(?i)/)                         : "String"
+]
+
+FILES.chooseDirectoryAndSave("Choose directory", "Choose where to store generated files") { dir ->
+  SELECTION.filter { it instanceof DasTable }.each { generate(it, dir) }
+}
+
+def generate(table, dir) {
+  def className = javaName(table.getName(), true)
+  def fields = calcFields(table)
+  packageName = dir.toString().replaceAll("\\\\", ".").replaceAll("/", ".").replaceAll("^.*src(\\.main\\.java\\.)?", "") + ";"
+  PrintWriter printWriter = new PrintWriter(new OutputStreamWriter(new FileOutputStream(new File(dir, className + ".java")), "UTF-8"))
+  printWriter.withPrintWriter {out -> generate(out, className, fields,table)}
+}
+
+def generate(out, className, fields,table) {
+  out.println "package $packageName"
+  out.println "/**"
+  out.println " *"+table.getComment()
+  out.println " */"
+  out.println "public class $className {"
+  out.println ""
+  fields.each() {
+    out.println "\t/**"
+    out.println "\t * ${it.commoent.toString()}"
+    out.println "\t */"
+    if (it.annos != "") out.println "  ${it.annos}"
+    out.println "\tprivate ${it.type} ${it.name};"
+  }
+  out.println ""
+  fields.each() {
+    out.println ""
+    out.println "  public ${it.type} get${it.name.capitalize()}() {"
+    out.println "    return ${it.name};"
+    out.println "  }"
+    out.println ""
+    out.println "  public void set${it.name.capitalize()}(${it.type} ${it.name}) {"
+    out.println "    this.${it.name} = ${it.name};"
+    out.println "  }"
+    out.println ""
+  }
+  out.println "}"
+}
+
+def calcFields(table) {
+  DasUtil.getColumns(table).reduce([]) { fields, col ->
+    def spec = Case.LOWER.apply(col.getDataType().getSpecification())
+    def typeStr = typeMapping.find { p, t -> p.matcher(spec).find() }.value
+    fields += [[
+                 name : javaName(col.getName(), false),
+                 type : typeStr,
+                 commoent: col.getComment(),
+                 annos: ""]]
+  }
+}
+
+def javaName(str, capitalize) {
+  def s = com.intellij.psi.codeStyle.NameUtil.splitNameIntoWords(str)
+    .collect { Case.LOWER.apply(it).capitalize() }
+    .join("")
+    .replaceAll(/[^\p{javaJavaIdentifierPart}[_]]/, "_")
+  capitalize || s.length() == 1? s : Case.LOWER.apply(s[0]) + s[1..-1]
+}
